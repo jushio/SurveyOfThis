0544c8d2262901b34bb6e0be6837c9c72f6bf877
jushio <ushio5gle@gmail.com>
2017-03-20 07:38:40 +0900

    あ

--- SurveyOfThis/Visitor.cs
+++ SurveyOfThis/Visitor.cs
@@ -2,14 +2,15 @@
 
 namespace SurveyOfThis
 {
-    public class PrintVisitor : Visitor
+    public class Visitor
     {
         public void visit(Parent wheel)
         {
             Console.WriteLine("Visiting parent");
         }
 
-        public void visit(Child child) {
+        public void visit(Child child)
+        {
             Console.WriteLine("Visiting child");
         }
     }
